ad2b8c59f4b4440409285ec2f91462a32e408834
Vanesa <vaneebg@gmail.com>
2022-06-16 09:32:27 +0200

    pintado local storage en home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,32 @@
 import './Home'
 
 const Home=()=>{
+
+const newsBack = JSON.parse(localStorage.getItem("NEWS"));
+const listNews=newsBack.map((notice) => { return ( <div className='noticeInvent'>
+      <h2>{notice.title}</h2>
+      <span>{notice.section}</span>
+      <p>{notice.author}</p>
+      <span>{notice.date}</span>
+      <div className="content">
+
+      <p>{notice.body}</p>
+      </div>
+      </div>)
+      })
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="image">
+
+      </div>
+      <div className="print">
+          {listNews}
+      </div>
+      <div className="image">
+        
+      </div>
+    </div>
   )
 }
 
